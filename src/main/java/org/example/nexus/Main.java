7f24f4ca2d2d496487c8d23b82514d37c44c7d0e
Emiliano Saenz <32196924+saenzemiliano@users.noreply.github.com>
2018-09-12 17:29:08 -0300

    Maven - Nexus Repository Manager

---
+++ src/main/java/org/example/nexus/Main.java
@@ -0,0 +1,11 @@
+package org.example.nexus;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    final static Logger logger = Logger.getLogger(Main.class);
+    public static void main(String [] args) {
+        logger.info("Nexus Repository Manager - Hello world!");
+    }
+}
